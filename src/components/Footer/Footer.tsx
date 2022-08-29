e39f8a139a76c64d0f6b7588ced8e13b7f2ccdfa
Nikita Abyshkin <ride1122@mail.ru>
2022-08-29 23:25:26 +0700

    Created footer layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import phone from './assets/phone-handset.svg';
+import facebook from './assets/facebook.svg';
+import instagram from './assets/instagram.svg';
+import payOne from './assets/mastercard.svg';
+import payTwo from './assets/visa.svg';
+import payThree from './assets/google-pay.svg';
+import payFour from './assets/apple-pay.svg';
+import phoneIcon from './assets/phone.svg';
+import emailIcon from './assets/email.svg';
+import logo from './assets/logo.svg';
+import s from './Footer.module.scss';
+import { Link } from 'react-router-dom';
+
+type TFooter = {};
+
+const Footer: React.FC<TFooter> = ({}) => {
+  const listOne = {
+    title: 'Product',
+    items: [
+      { value: 'New Arrival', path: '/' },
+      { value: 'Living Room', path: '/' },
+      { value: 'Kitchen & Dining Room', path: '/' },
+      { value: 'Bed Room', path: '/' },
+      { value: 'Accent', path: '/' },
+      { value: 'Occasional Table', path: '/' },
+      { value: 'Office/Home Office', path: '/' },
+      { value: 'Outdoor Furniture', path: '/' },
+    ],
+  };
+  const listTwo = {
+    title: 'Help',
+    items: [
+      { value: 'About Us', path: '/' },
+      { value: 'Contact', path: '/' },
+    ],
+  };
+  return (
+    <div className={s.container}>
+      <div className={s.helpBlock}>
+        <div className={s.wrapper}>
+          <div className={s.title}>How Can We Help You?</div>
+          <div className={s.text}>
+            We pride ourselves on superior customer service. Speak to a member of our sales team to
+            help with your order.
+          </div>
+          <div className={s.email}>Get In Touch support@Meridian.com</div>
+        </div>
+        <div className={s.wrapper}>
+          <button className={s.button}>
+            <img src={phone} alt="phone" /> 01789 339533
+          </button>
+          <div className={s.time}>8am to 6pm, 7 days a week</div>
+        </div>
+      </div>
+      <div className={s.main}>
+        <div className={s.logoBlock}>
+          <div className={s.logo}>
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+        <div className={s.menuBlock}>
+          <ul>
+            <li className={s.title}>Head Office</li>
+            <li className={s.state}>32 Plum St, Trenton, NJ 08638, United States</li>
+            <li>
+              <img src={phoneIcon} alt="phone" />
+              01789 730695
+            </li>
+            <li>
+              <img src={emailIcon} alt="email" />
+              support@Meridian.com
+            </li>
+          </ul>
+          <ul>
+            <li className={s.title}>{listOne.title}</li>
+            {listOne.items.map((obj, index) => (
+              <li key={index}>
+                <Link to={obj.path}>{obj.value}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            <li className={s.title}>{listTwo.title}</li>
+            {listTwo.items.map((obj, index) => (
+              <li key={index}>
+                <Link to={obj.path}>{obj.value}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={s.emailBlock}>
+            <li className={s.title}>Join Meridian for Exclusive Offers</li>
+            <li className={s.inputemail}>
+              <input type="email" placeholder="Email address" />
+              <button type="submit"></button>
+            </li>
+            <li className={s.text}>
+              Our newsletter is packed full of style ideas, new products and exclusive disounts. We
+              are GDPR compliant, your information is secure with us.
+            </li>
+          </ul>
+        </div>
+        <div className={s.policyBlock}>
+          <div className={s.policy}>
+            <ul>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+              <li>Shipping Policy</li>
+            </ul>
+            <div className={s.social}>
+              <div className={s.facebook}>
+                <img src={facebook} alt="facebook" />
+              </div>
+              <div className={s.instagram}>
+                <img src={instagram} alt="instagram" />
+              </div>
+            </div>
+          </div>
+          <div className={s.payment}>
+            <img src={payOne} alt="mastercard" />
+            <img src={payTwo} alt="visa" />
+            <img src={payThree} alt="google pay" />
+            <img src={payFour} alt="apple pay" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 
 type TMainLayout = {};
 
@@ -9,6 +10,7 @@ const MainLayout: React.FC<TMainLayout> = ({}) => {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
