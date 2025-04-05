1cd181b12b564b1a7054ab2ad3253577bb62b48e
PuuPSEK <zai.nikita@mail.ru>
2025-04-05 11:11:03 +0700

    111

--- src/pages/LoginPage/components/LoginForm.tsx
+++ src/pages/LoginPage/components/LoginForm.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './LoginForm.module.scss';
+import React from "react";
+import styles from "./LoginForm.module.scss";
 
 const LoginForm = () => {
   return (
